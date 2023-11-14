ad8baafe53b51faeba4e2502bcfd17c0e8f4e0bf
Nicholas <aigberuan6@gmail.com>
2023-11-13 17:27:44 -0800

    one

--- Binary Search.py
+++ Binary Search.py
@@ -1,6 +1,6 @@
 def binary_search(arr, target):
-    
-   
-
-
-
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            
